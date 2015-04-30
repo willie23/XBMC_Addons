d99528a42af555640f093988b391f500d0aaf7e0
Lunatixz <airies1153@aol.com>
2015-04-30 16:01:15 -0400

    PTVL Web Interface init

---
+++ webinterface.ptvlmanager/app/components/ptvl/services/index.js
@@ -0,0 +1,7 @@
+define([
+    './dialogs',
+    './../min/services/settingsXml.min',
+    './../min/services/plugins.min',
+    './../min/services/rules.min',
+    './../min/services/lock.min'
+], function () {});
