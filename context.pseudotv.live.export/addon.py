0951c5b2f3fcd4978e8dd0c3263947edb1fff522
Lunatixz <Kgraer@aol.com>
2015-07-23 13:32:20 -0700

    PTVL Context, USTVnow v.10.0.5

---
+++ context.pseudotv.live.export/addon.py
@@ -0,0 +1,51 @@
+#   Copyright (C) 2015 Kevin S. Graer
+#
+#
+# This file is part of PseudoTV Live.
+#
+# PseudoTV is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PseudoTV is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PseudoTV Live.  If not, see <http://www.gnu.org/licenses/>.
+
+import xbmc, xbmcaddon
+
+if __name__ == '__main__':	
+
+    # PseudoTV Live Globals
+    ADDON_ID = 'script.pseudotv.live'
+    REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+    ADDON_ID = REAL_SETTINGS.getAddonInfo('id')
+    ADDON_NAME = REAL_SETTINGS.getAddonInfo('name')
+    ADDON_PATH = (REAL_SETTINGS.getAddonInfo('path').decode('utf-8'))
+    ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
+    
+    # InfoLabel Parameters  
+    label    = xbmc.getInfoLabel('ListItem.Label')
+    path     = xbmc.getInfoLabel('ListItem.FolderPath')
+    filename = xbmc.getInfoLabel('ListItem.FilenameAndPath')
+    plot = xbmc.getInfoLabel('ListItem.Plot')
+    plotOutline = xbmc.getInfoLabel('ListItem.PlotOutline')
+    description = xbmc.getInfoLabel('ListItem.Property(Addon.Description)')
+    isPlayable = xbmc.getInfoLabel('ListItem.Property(IsPlayable)').lower() == 'true'
+    isFolder = xbmc.getCondVisibility('ListItem.IsFolder') == 1
+    
+    if not plot:
+        if plotOutline:
+            description = description
+    else:
+        description = plot
+            
+    data = ['Label='+label,'Path='+path,'FileName='+filename,'Description='+description,'isPlayable='+str(isPlayable),'isFolder='+str(isFolder)]
+    
+    # Pass arguments to PseudoTV Live
+    xbmc.executebuiltin("RunScript("+ADDON_PATH+"/capture.py, %s)" %str(data))
+    
