75cc937290560c5aedd37c62eeeca168ded1994d
Lunatixz <Kgraer@gmail.com>
2016-05-16 21:52:04 -0400

    PTVL v.0.7.6b

---
+++ script.pseudotv.live/service.py
@@ -0,0 +1,63 @@
+﻿#   Copyright (C) 2015 Kevin S. Graer
+#
+#
+# This file is part of PseudoTV Live.
+#
+# PseudoTV Live is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PseudoTV Live is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PseudoTV Live.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, xbmc, xbmcgui, xbmcaddon, xbmcvfs
+
+# Plugin Info
+ADDON_ID = 'script.pseudotv.live'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME = REAL_SETTINGS.getAddonInfo('name')
+ADDON_PATH = REAL_SETTINGS.getAddonInfo('path').decode('utf-8')
+THUMB = (xbmc.translatePath(os.path.join(ADDON_PATH, 'resources', 'images')) + '/' + 'icon.png')
+AUTOSTART_TIMER = [0,5,10,15,20,25,30][int(REAL_SETTINGS.getSetting('AutoStart'))]
+
+def handle_wait(time_to_wait): #*Thanks enen92
+    dlg = xbmcgui.DialogProgress()
+    dlg.create("PseudoTV Live", 'AutoStart')
+    secs=0
+    percent=0
+    increment = int(100 / time_to_wait)
+    cancelled = False
+    while secs < time_to_wait:
+        secs += 1
+        percent = increment*secs
+        secs_left = str((time_to_wait - secs))
+        dlg.update(percent,"PseudoTV Live will autoStart in " + str(secs_left) + " seconds, Cancel?")
+        xbmc.sleep(1000)
+        if (dlg.iscanceled()):
+            cancelled = True
+            break
+    if cancelled == True:
+        return False
+    else:
+        dlg.close()
+        return True
+
+if AUTOSTART_TIMER != 0:
+    xbmc.log('script.pseudotv.live-Service: autostart')
+    xbmc.executebuiltin("Notification(%s, %s, %d, %s)" % (ADDON_NAME, "AutoStart Enabled", (AUTOSTART_TIMER * 1000)/2, THUMB))
+    if handle_wait(AUTOSTART_TIMER) == True:
+        xbmc.executebuiltin('RunScript("' + ADDON_PATH + '/default.py' + '")')
+        
+# while (not xbmc.abortRequested):
+    # xbmc.sleep(100)
+    # if xbmcgui.Window(10000).getProperty('PseudoTVRunning') == 'True':
+        # if xbmc.getCondVisibility('Window.IsActive(busydialog)') == 1:
+            # xbmc.executebuiltin('Dialog.Close(busydialog)')
+        # if xbmc.getCondVisibility('Window.IsActive(infodialog)') == 1:
+            # xbmc.executebuiltin('Dialog.Close(infodialog)')     
